3ca2bccc0f86e3bcd0e4595512fed6955bc63e1b
Victor Le <selftaughtvic@gmail.com>
2019-12-05 17:20:42 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/TileGame5/dev/techknowcoder/display/Display.class
    #	out/production/TileGame5/dev/techknowcoder/tilegame/Game.class
    #	out/production/TileGame5/dev/techknowcoder/tilegame/Launcher.class
    #	src/dev/techknowcoder/tilegame/Game.java

--- src/dev/techknowcoder/tilegame/Game.java
+++ src/dev/techknowcoder/tilegame/Game.java
@@ -1,7 +1,13 @@
 package dev.techknowcoder.tilegame;
 
+import dev.techknowcoder.tilegame.gfx.Assets;
+import dev.techknowcoder.tilegame.gfx.SpriteSheet;
 import dev.techknowcoder.display.Display;
+import dev.techknowcoder.tilegame.gfx.ImageLoader;
 
+import java.awt.image.BufferedImage;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 public class Game implements Runnable
 {
     private Display display;
@@ -12,6 +18,12 @@ public class Game implements Runnable
     private boolean running = false;
     private Thread thread;
 
+	private BufferStrategy bs;
+	private Graphics g;
+
+	private BufferedImage testImage;
+	private SpriteSheet sheet;
+
     public Game(String title, int width, int height)
     {
         this.width = width;
@@ -21,13 +33,27 @@ public class Game implements Runnable
 
     private void init() {
     	display = new Display(title, width, height);
+		Assets.init();
     }
 
     private void tick(){
 
     }
     private void render(){
+		bs = display.getCanvas().getBufferStrategy();
+		if(bs == null){
+			display.getCanvas().createBufferStrategy(3);
+			return;
+		}
 
+		g = bs.getDrawGraphics();
+
+		g.clearRect(0,0, width, height);
+		//Draw Here!
+		g.drawImage(Assets.grass, 10, 10, null);
+		//End Drawing!
+		bs.show();
+		g.dispose();
     }
     public void run()
     {
